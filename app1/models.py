7deda71dcc34ed8a2c5c6998dfadfac9c9b0845d
J-Atulya <atulyajayakrishnan@gmail.com>
2023-06-06 19:24:26 +0530

    Committing all the changes

--- app1/models.py
+++ app1/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class feature(models.Model):
     name = models.CharField(max_length=100)
-    details = models.CharField(max_length=250)
+    details = models.CharField(max_length=500)
