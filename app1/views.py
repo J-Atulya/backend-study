87dbf030c0a5352e2de8598dd963920640e406f9
J-Atulya <atulyajayakrishnan@gmail.com>
2023-06-06 21:39:35 +0530

    changes

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.models import User, auth
+from django.contrib.auth.models import User
+from django.contrib import auth
 from django.contrib import messages
 from .models import feature
 
@@ -13,7 +14,7 @@ def register(request):
     
     if request.method == 'POST':
         # if the user submits something
-        username = request.POST['username']
+        uname = request.POST['username']
         # this stores the value given to textbox username to this variable
         mail = request.POST['email']
         password = request.POST['password']
@@ -21,19 +22,19 @@ def register(request):
 
         if password == pass2:
             #checking if the email already exists
-            if User.objects.filter(email='mail').exists():
+            if User.objects.filter(username=uname).exists():
+                messages.info(request,'Username Already Used' )
+                return redirect('register')
+            
+            elif User.objects.filter(email=mail).exists():
                 messages.info(request, 'Email Already Used')
                 # redirecting back to the refreshed form page with 
-                return redirect('register')   
-
-            elif User.objects.filter(username='username').exists():
-                messages.info(request,'Username Already Used' )
-                return redirect('register') 
-
+                return redirect('register')    
+            
             else:
                 # if the email and username is not already in use
-                user1 = User.objects.create_user(username=username, email=mail, password=pass2)
-                user1.save()
+                user = User.objects.create_user(username=uname, email=mail, password=pass2)
+                user.save()
                 return redirect('login')
             
         else:
