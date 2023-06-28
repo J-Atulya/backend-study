2f64e829a8f40b8c61bf83b128cb1f3327830991
J-Atulya <atulyajayakrishnan@gmail.com>
2023-06-29 00:02:56 +0530

    Dynamic Routing

--- app1/url1.py
+++ app1/url1.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('counter', views.counter, name='counter'),
     path('register', views.register, name='register'),
     path('loginpage', views.loginpage, name = 'loginpage'),
-    path('logout', views.logout, name='logout')
+    path('logout', views.logout, name='logout'),
+    #str-> string type; pk -> variable; next code creates a dynamic url => diff for diff user
+    path('post/<str:pk>', views.post, name='post')
 ]
